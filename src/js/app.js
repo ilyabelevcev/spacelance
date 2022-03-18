a46be87397e7ae39d282e168f9609e17305046f0
ilyabelevcev <91212003+ilyabelevcev@users.noreply.github.com>
2022-03-18 03:27:30 +0300

    spacelancer

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 import * as flsFunctins from "./modules/functions.js"
+import $ from "jquery"
+import "slick-carousel"
 
 flsFunctins.isWebp();
+
+$(".carousel").slick({
+   infinite: false,
+   slidesToShow: 3, 
+   speed: 800,
+   slidesToScroll: 3,
+   centerPadding: '0',
+   prevArrow: $('.prev'),
+   nextArrow: $('.next'),
+   appendArrows: $('.works__slider-btn'),
+})

c96ce8cfe4a003021aa345bcbffcc27d87079a89
ilyabelevcev <91212003+ilyabelevcev@users.noreply.github.com>
2022-03-20 00:55:27 +0300

    spacelance

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,33 @@ $(".carousel").slick({
    prevArrow: $('.prev'),
    nextArrow: $('.next'),
    appendArrows: $('.works__slider-btn'),
+   responsive: [
+      {
+         breakpoint: 1199,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+         }
+      },
+      // {
+      //    breakpoint: 600,
+      //    settings: {
+      //       slidesToShow: 2,
+      //       slidesToScroll: 2
+      //    }
+      // },
+      // {
+      //    breakpoint: 480,
+      //    settings: {
+      //       slidesToShow: 1,
+      //       slidesToScroll: 1
+      //    }
+      // }
+   ]
 })
+
+$(document).ready(function() {
+   $('.burger-menu').click(function() {
+      $('.burger-menu').toggleClass('open-menu')
+   });
+});
